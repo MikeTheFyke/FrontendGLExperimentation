3177ebb2c466d1b7b533b0a4529a9cfb5f066642
MikeTheFyke <mikefyke@hotmail.com>
2020-08-17 16:41:28 -0400

    Added color and depth buffer color

--- logic/app.js
+++ logic/app.js
@@ -13,9 +13,12 @@ var InitDemo = function () {
         alert('Your Browser does not support WebGL');
     }
 
-// This is to change dynamically the size of the canvas element to fit the device screen size 
+/// This is to change dynamically the size of the canvas element to fit the device screen size 
     // canvas.width = window.innerWidth;
     // canvas.height = window.innerHeight;
     // gl.viewport (0,0, window.innerWidth, window.innerHeight);
+
+    gl.clearColor (0.75,0.85,0.8,1.0) // set the color of the paint
+    gl.clear(); // paints the defined color : Color Buffer & Depth Buffer etc.
 
 };
