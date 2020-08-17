9713f8b0d2bba6fd631262c30cb1af4053701169
MikeTheFyke <mikefyke@hotmail.com>
2020-08-17 16:33:09 -0400

    Added canvas and context along with warning for support of webgl in app js

--- logic/app.js
+++ logic/app.js
@@ -1,3 +1,15 @@
 var InitDemo = function () {
     console.log('This is working');
+
+    var canvas = document.getElementById('triangle-canvas');
+    var gl = canvas.getContext ('webgl');
+
+    if (!gl){
+        console.log('WebGL not supported, falling back to experimental gl');
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if (!gl) {
+        alert('Your Browser does not support WebGL');
+    }
 };
