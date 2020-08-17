8e45135e580266837ad7db9f9138c4c4ac0ca438
MikeTheFyke <mikefyke@hotmail.com>
2020-08-17 16:37:59 -0400

    Added code to dynamically change canvas width and height to fit device window size

--- logic/app.js
+++ logic/app.js
@@ -1,5 +1,5 @@
 var InitDemo = function () {
-    console.log('This is working');
+    console.log('Demo is initializing');
 
     var canvas = document.getElementById('triangle-canvas');
     var gl = canvas.getContext ('webgl');
@@ -12,4 +12,10 @@ var InitDemo = function () {
     if (!gl) {
         alert('Your Browser does not support WebGL');
     }
+
+// This is to change dynamically the size of the canvas element to fit the device screen size 
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+    // gl.viewport (0,0, window.innerWidth, window.innerHeight);
+
 };
