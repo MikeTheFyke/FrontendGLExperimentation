f4c420d0668041a549fe68664fc6e3db32276dce
MikeTheFyke <mikefyke@hotmail.com>
2020-08-20 14:36:31 -0400

    Updated with all 13 frames per T and P

--- logic/canvas/TandP.js
+++ logic/canvas/TandP.js
@@ -1,13 +1,17 @@
 var canvas = document.getElementById('TandP-canvas');
 var ctx = canvas.getContext('2d');
 var imgP = document.getElementById('P01-image');
-var framesPerSecond = 10;
+var imgT = document.getElementById('T01-image');
+var framesPerSecond = 30;
+
+var mouseX;
+var mouseY;
 
 function calculateMousePos(evt){      // an event fires when mouse moves
     var rect = canvas.getBoundingClientRect();
     var root = document.documentElement;
-    var mouseX = evt.clientX - rect.left - root.scrollLeft;
-    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    mouseX = evt.clientX - rect.left - root.scrollLeft;
+    mouseY = evt.clientY - rect.top - root.scrollTop;
     return {
       x:mouseX,   // Object Literal
       y:mouseY    // Object Literal
@@ -26,8 +30,27 @@ window.onload = function() {
             Ycord = mousePos.y;
        }
     );
+    canvas.addEventListener("click", function(){
+        TFart();
+    });
 }
 
 function drawEverything () {
-    ctx.drawImage(imgP,10,10)
+    ctx.drawImage(imgP,400,10)
+    ctx.drawImage(imgT,100,10)
 }
+
+function TFart(){
+    for (var x = 1; x <= 13; x++){
+        ctx.fillStyle = "white";
+        ctx.beginPath()
+        ctx.rect(0,0,800,600);
+        ctx.fill();
+        imgT = document.getElementById("T" + x + "-fart");
+        imgP = document.getElementById("P" + x + "-fart");
+        ctx.drawImage(imgP,400,10)
+        ctx.drawImage(imgT,100,10)
+        console.log(x);
+    }
+}
+
