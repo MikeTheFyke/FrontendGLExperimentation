a8475a1aae9cb261715527470de4d7f077fe4d56
MikeTheFyke <mikefyke@hotmail.com>
2020-08-21 14:54:48 -0400

    Started threejs blocks js scene,camera,renderer

---
+++ logic/jsBlocks.js
@@ -0,0 +1,9 @@
+// Three base ThreeJS elements 1.Scene 2.Camera 3.Renderer
+var scene = new THREE.Scene ();
+
+var camera = new THREE.PerspectiveCamera (75,window.innerWidth/window.innerHeight,0.1,1000);
+
+var renderer = new THREE.WebGLRenderer (antialias: true);
+
+renderer.setClearColor("#e5e5e5"); // background color light grey;
+renderer.setSize(window.innerWidth, window.innerHeight);
