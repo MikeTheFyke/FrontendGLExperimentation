a25163a0a1972d1713c0674d8e5d05cdb2fc2903
MikeTheFyke <mikefyke@hotmail.com>
2020-08-21 15:13:19 -0400

    ThreeJS is loading in the browser after installing live server in visual studio

--- logic/jsBlocks.js
+++ logic/jsBlocks.js
@@ -3,7 +3,9 @@ var scene = new THREE.Scene ();
 
 var camera = new THREE.PerspectiveCamera (75,window.innerWidth/window.innerHeight,0.1,1000);
 
-var renderer = new THREE.WebGLRenderer (antialias: true);
+var renderer = new THREE.WebGLRenderer ({antialias: true});
 
 renderer.setClearColor("#e5e5e5"); // background color light grey;
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
