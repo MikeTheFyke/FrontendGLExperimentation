f8b611d73e3dbfb0af59565dc8167967a522ac2c
MikeTheFyke <mikefyke@hotmail.com>
2020-08-22 18:25:50 -0400

    Loaded scene and camera into render

--- logic/jsBlocks.js
+++ logic/jsBlocks.js
@@ -2,6 +2,7 @@
 var scene = new THREE.Scene ();
 
 var camera = new THREE.PerspectiveCamera (75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z = 5;
 
 var renderer = new THREE.WebGLRenderer ({antialias: true});
 
@@ -15,7 +16,7 @@ document.body.appendChild(renderer.domElement);
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectMatrix();
+    camera.updateProjectionMatrix();
 })
 
 renderer.render(scene, camera);
