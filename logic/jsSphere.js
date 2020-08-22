f227446efde9edb19c1c5a37d186a101db868720
MikeTheFyke <mikefyke@hotmail.com>
2020-08-22 18:39:24 -0400

    Added links for three js sphere and blocks to all pages

---
+++ logic/jsSphere.js
@@ -0,0 +1,34 @@
+// Three base ThreeJS elements 1.Scene 2.Camera 3.Renderer
+var scene = new THREE.Scene ();
+
+var camera = new THREE.PerspectiveCamera (75,window.innerWidth/window.innerHeight,0.1,1000);
+camera.position.z = 5; // Lower number will zoom the camera in, higher zoom out
+
+var renderer = new THREE.WebGLRenderer ({antialias: true});
+
+renderer.setClearColor("#e5e5e5"); // background color light grey;
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+
+// Allow renderer and camera to adjust dynamically with browser window adjustment
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
+
+// Geometry or form of the object + Material of object
+var geometry = new THREE.SphereGeometry(1, 10, 10); // (Radius, Width Segments, Height Segments)
+var material = new THREE.MeshLambertMaterial({color: 0xFFCC00}); 
+
+var mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+var light = new THREE.PointLight (0xFFFFFF, 1, 500); // Color (white), Intensity, Distance
+light.position.set(10,0,25);
+scene.add(light);
+
+renderer.render(scene, camera);
