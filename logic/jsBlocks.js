7dcaa7067dcd524367777028a0ad9e0ba664e424
MikeTheFyke <mikefyke@hotmail.com>
2020-08-22 18:34:41 -0400

    Created sphere 3js html and logic

--- logic/jsBlocks.js
+++ logic/jsBlocks.js
@@ -2,7 +2,7 @@
 var scene = new THREE.Scene ();
 
 var camera = new THREE.PerspectiveCamera (75,window.innerWidth/window.innerHeight,0.1,1000);
-camera.position.z = 5;
+camera.position.z = 5; // Lower number will zoom the camera in, higher zoom out
 
 var renderer = new THREE.WebGLRenderer ({antialias: true});
 
