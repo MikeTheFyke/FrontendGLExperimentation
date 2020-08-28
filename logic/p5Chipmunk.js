5afbd08a4ef8f10ce00f6a518bd7f3084a2632f0
MikeTheFyke <mikefyke@hotmail.com>
2020-08-28 14:47:31 -0400

    Created a second spritesheet for chipmunks right to left

--- logic/p5Chipmunk.js
+++ logic/p5Chipmunk.js
@@ -2,16 +2,19 @@ let spriteSheet;
 let spriteData;
 
 let animation = [];
+let animationB = [];
 
 // let chipmunk; // Single Chipmunk
 
 let chipmunks = [];
+let chipmunksB = [];
 
 var windowW = window.innerWidth;
 console.log("window width is " + windowW);
 
 function preload(){
     spriteSheet = loadImage('../images/SpriteSheetChipmunk.png');
+    spriteSheetB = loadImage('../images/SpriteSheetChipmunkB.png');
     spriteData = loadJSON('../logic/chipmunkFrames.json');
 }
 
@@ -21,12 +24,15 @@ function setup(){
     for (let i = 0; i < frames.length; i++){
         let pos = frames[i].position; // Find Position of frame
         let img = spriteSheet.get(pos.x, pos.y, pos.w, pos.h);
+        let imgB = spriteSheetB.get(pos.x, pos.y, pos.w, pos.h);
         animation.push(img);
+        animationB.push(imgB);
     }
     // chipmunk = new Sprite(animation, 100, 100, 1); // new sprite from p5ChipmunkSprite and a speed of 1 // added an x 100 and a y 100 // A single Chipmunk
     for (let i = 0; i < 5; i ++){ // Multiple Chipmunks
         // chipmunks[i] = new Sprite(animation, 0, i * 50, 1) they run at the same speed
-        chipmunks[i] = new Sprite(animation, -288, i * 20, random(1.25, 2), windowW); // creates multiple random speeds to run
+        chipmunks[i] = new Sprite(animation,animationB, -288, windowW + 288, i * 20, random(1.25, 2), random(1.25, 2), windowW); // creates multiple random speeds to run
+        // chipmunksB[i] = new Sprite(animationB, -288, i * 20, random(1.25, 2), windowW);
     }
 }
 
@@ -38,6 +44,10 @@ function draw(){
     for (let chipmunk of chipmunks){
         chipmunk.show();
         chipmunk.animate();
+    }
+    for (let chipmunkB of chipmunksB){
+        chipmunkB.show();
+        chipmunkB.animate();
     }
     // chipmunk.show(); // single Chipmunk
     // chipmunk.animate(); // Create a animate and show function in p5ChipmunkSprite // single Chipmunk
