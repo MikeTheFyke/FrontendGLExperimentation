675b6c3fb07a0f05e71d8e9edc0c34c4fbe4e0cb
MikeTheFyke <mikefyke@hotmail.com>
2020-09-04 15:17:59 -0400

    All three set are animating

--- public/logic/gsapAMNS.js
+++ public/logic/gsapAMNS.js
@@ -4,26 +4,23 @@ new fullpage('#fullpage', {
     onLeave: (origin, destination, direction) =>{
         const section = destination.item;
         const title = section.querySelector('h1');
+        const subtitle = section.querySelector('h2');
+        const description = section.querySelector('p');
+
+        const sigil = document.querySelectorAll('.sigil');
+        const avatar1 = document.querySelectorAll('.avatar1');
+        const avatar2 = document.querySelectorAll('.avatar2');
+        const avatar2B = document.querySelectorAll('.avatar2B');
+
         const tl = new TimelineMax({ delay: 0.5});
         tl.fromTo(title, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
+        tl.fromTo(subtitle, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
+        tl.fromTo(description, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
 
-        if (destination.index === 0){
-            const sigil = document.querySelectorAll('.sigil');
-            const avatar1 = document.querySelectorAll('.avatar1');
-            const avatar2 = document.querySelectorAll('.avatar2');
-
-            tl.fromTo(sigil, 0.5, {x: '175%'}, {x: '-50%'});
-            tl.fromTo(avatar1, 0.5, {x: '-350%'}, {x: '-20%'})
-            tl.fromTo(avatar2, 0.5, {x: '175%'}, {x: '-20%'})
-        }
-
-        if (destination.index === 1){
-            const sigil = document.querySelectorAll('.sigil');
-            const avatar = document.querySelectorAll('.avatar2B');
-
-            tl.fromTo(sigil, 0.7, {x: '175%'}, {x: '-50%'});
-            tl.fromTo(avatar, 0.7, {x: '-350%'}, {x: '-20%'});
-        }
+        tl.fromTo(sigil, 0.5, {x: '175%'}, {x: '-50%'});
+        tl.fromTo(avatar1, 0.5, {x: '-350%'}, {x: '-20%'});
+        tl.fromTo(avatar2, 0.5, {x: '200%'}, {x: '-20%'});
+        tl.fromTo(avatar2B, 0.5, {x: '-350%'}, {x: '-20%'});
 
     }
 });
