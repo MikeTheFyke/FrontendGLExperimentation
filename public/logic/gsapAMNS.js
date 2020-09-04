aab1bc682526470b0103a9b689f3e5e937aaf919
MikeTheFyke <mikefyke@hotmail.com>
2020-09-04 14:57:19 -0400

    neckott section is animating avatar and sigil

--- public/logic/gsapAMNS.js
+++ public/logic/gsapAMNS.js
@@ -6,5 +6,22 @@ new fullpage('#fullpage', {
         const title = section.querySelector('h1');
         const tl = new TimelineMax({ delay: 0.5});
         tl.fromTo(title, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
+
+        if (destination.index === 0){
+            const sigil = document.querySelectorAll('.sigil');
+            const avatar1 = document.querySelectorAll('.avatar1');
+            const avatar2 = document.querySelectorAll('.avatar2');
+
+            tl.fromTo(sigil, 0.7, {x: '175%'}, {x: '-50%'});
+            tl.fromTo(avatar, 0.7, {x: '-350%'}, {x: '-20%'})
+        }
+
+        if (destination.index === 1){
+            const sigil = document.querySelectorAll('.sigil');
+            const avatar = document.querySelectorAll('.avatar2B');
+
+            tl.fromTo(sigil, 0.7, {x: '175%'}, {x: '-50%'});
+            tl.fromTo(avatar, 0.7, {x: '-350%'}, {x: '-20%'})
+        }
     }
 });
