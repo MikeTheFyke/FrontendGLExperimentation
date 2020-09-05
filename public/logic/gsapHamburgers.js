26dc550705c0a194397c61552d3d893b1232fded
MikeTheFyke <mikefyke@hotmail.com>
2020-09-04 21:12:30 -0400

    Finished with the hamburgers gsap

--- public/logic/gsapHamburgers.js
+++ public/logic/gsapHamburgers.js
@@ -8,4 +8,10 @@ var rule = CSSRulePlugin.getRule("span:after");
 var tl = gsap.timeline({default: {duration: 1}})
 
 tl.from(".anim1", {y: -50, stagger: .6, opacity: 0})
-.to(rule, {duration: 1.8, cssRule: {scaleY: 0}})
+.to(rule, {duration: 1.8, cssRule: {scaleY: 0}}, "-=2.2")
+.from("aside", {backgroundPosition: '200px 0px', opacity: 0}, "-=1.5")
+.from("#CowEating", {y: 50, opacity: 0}, "-=.5")
+
+document.getElementById('cta').addEventListener('click', () => {
+    tl.reversed() ? tl.play() : tl.reverse();
+})
