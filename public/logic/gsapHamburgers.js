0130ebe7bd4203a3db1d206347d827442168be6a
MikeTheFyke <mikefyke@hotmail.com>
2020-09-04 21:02:02 -0400

    Added Gsap CSSRULE plugin

--- public/logic/gsapHamburgers.js
+++ public/logic/gsapHamburgers.js
@@ -1,3 +1,11 @@
-gsap.from('.gsap-content', {opacity :0, duration: 1, y: -50});
-gsap.from('#CowEating', {duration: 1, y: 30, opacity :0, delay: 1.4});
-gsap.from('aside', {duration: 1, backgroundPosition: '200px 0px', opacity :0});
+var rule = CSSRulePlugin.getRule("span:after");
+
+// gsap.from('.anim1', {opacity :0, duration: 1, y: -50, stagger: 0.6});
+// gsap.from('#CowEating', {duration: 1, y: 30, opacity :0, delay: 1.4});
+// gsap.to(rule, {cssRule: {scaleY: 0}, duration:1});
+// gsap.from('aside', {duration: 1, delay: 1.1, backgroundPosition: '200px 0px', opacity :0});
+
+var tl = gsap.timeline({default: {duration: 1}})
+
+tl.from(".anim1", {y: -50, stagger: .6, opacity: 0})
+.to(rule, {duration: 1.8, cssRule: {scaleY: 0}})
