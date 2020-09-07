f41495fb359cd29d9f05e7882fda566357a89652
MikeTheFyke <mikefyke@hotmail.com>
2020-09-06 20:29:39 -0400

    Cleaned up canvasWalk js to start a new

--- public/logic/canvasWalk.js
+++ public/logic/canvasWalk.js
@@ -5,39 +5,9 @@ var showingWelcomeBanner = true;
 
 var WelcomeBanner = document.getElementById('welcomeBanner');
 
-
-let spriteSheet;
-let spriteData;
-
-let animation = [];
-
-// let chipmunk; // Single Chipmunk
-
-let chipmunks = [];
-
 var windowW = canvas.width;
 var windowH = canvas.height;
 
-function preload(){
-  spriteSheet = loadImage('../images/SpriteSheetChipmunk.png');
-  spriteData = loadJSON('../logic/chipmunkFrames.json');
-  console.log("Preload has run");
-}
-
-function setup(){
-  let frames = spriteData.frames;
-  for (let i = 0; i < frames.length; i++){
-      let pos = frames[i].position; // Find Position of frame
-      let img = spriteSheet.get(pos.x, pos.y, pos.w, pos.h);
-      animation.push(img);
-  }
-  // chipmunk = new Sprite(animation, 100, 100, 1); // new sprite from p5ChipmunkSprite and a speed of 1 // added an x 100 and a y 100 // A single Chipmunk
-  for (let i = 0; i < 5; i ++){ // Multiple Chipmunks
-      // chipmunks[i] = new Sprite(animation, 0, i * 50, 1) they run at the same speed
-      chipmunks[i] = new Sprite(animation, -288, i * 20, random(1.25, 2), windowW); // creates multiple random speeds to run
-      // chipmunksB[i] = new Sprite(animationB, -288, i * 20, random(1.25, 2), windowW);
-  }
-}
 
 window.onload = function() {  
     console.log("CanvasWidth : " + windowW);
@@ -65,7 +35,6 @@ function calculateMousePos(evt){      // an event fires when mouse moves
 function handleMouseClick(evt){
   if(showingWelcomeBanner) {
     showingWelcomeBanner = false;
-    draw();
   }
 }
 
@@ -73,25 +42,8 @@ function handleMouseClick(evt){
 function drawEverything() {
     ctx.fillStyle = "Black";
     ctx.fillRect(0,0,900,650);
-
     if (showingWelcomeBanner) {
         ctx.drawImage(WelcomeBanner, 350, 300);
         return;
     }
 }
-
-function draw(){
-  background( '#FFFFFF');
-  // image(spriteSheet,0,0); // displays all frames
-  // image(animation[5],0,0); // displays one at a time
-  // image(animation[frameCount % animation.length],0,0); // animation[frameCount % animation.length] displays all frames = ANIMATION! // Modjeo Operator
-  for (let chipmunk of chipmunks){
-      chipmunk.show();
-      chipmunk.animate();
-  }
-
-  // chipmunk.show(); // single Chipmunk
-  // chipmunk.animate(); // Create a animate and show function in p5ChipmunkSprite // single Chipmunk
-}
-
-//Ren and stimpy 1 10 32
