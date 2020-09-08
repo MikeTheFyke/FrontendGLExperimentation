0420af64ca7ce88e31b0f5fb4c700cb9bf80dc8d
MikeTheFyke <mikefyke@hotmail.com>
2020-09-08 17:48:20 -0400

    implemented gsap transitions to examples

--- public/logic/sandboxLogic.js
+++ public/logic/sandboxLogic.js
@@ -3,18 +3,18 @@ new fullpage('#preview', {
     navigation: true,
     onLeave: (origin, destination, direction) =>{
         const section = destination.item;
-        // const title = section.querySelector('h1');
+        const title = section.querySelector('.section-header');
         // const subtitle = section.querySelector('h2');
-        // const description = section.querySelector('p');
+        const description = section.querySelector('p');
 
-        // const sigil = document.querySelectorAll('.sigil');
+        const sigil = document.querySelectorAll('.section-canvas');
 
         const tl = new TimelineMax({ delay: 0.5});
-        // tl.fromTo(title, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
+        tl.fromTo(title, 0.5, {x: "-50", opacity: 0}, {x: 0, opacity: 1});
         // tl.fromTo(subtitle, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
-        // tl.fromTo(description, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
+        tl.fromTo(description, 0.5, {y: "50", opacity: 0}, {y: 0, opacity: 1});
 
-        // tl.fromTo(sigil, 0.5, {x: '175%'}, {x: '-50%'});
+        tl.fromTo(sigil, 0.5, {x: '-175%'}, {x: '0%'});
         // tl.fromTo(avatar1, 0.5, {x: '-350%'}, {x: '-20%'});
         // tl.fromTo(avatar2, 0.5, {x: '200%'}, {x: '-20%'});
         // tl.fromTo(avatar2B, 0.5, {x: '-350%'}, {x: '-20%'});
