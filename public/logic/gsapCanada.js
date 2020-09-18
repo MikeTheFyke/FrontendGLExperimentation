640b01aa18be2ee24e95a541529d7c3765682472
MikeTheFyke <mikefyke@hotmail.com>
2020-09-18 15:08:28 -0400

    Created canada flag png and add more css to canadaEh

--- public/logic/gsapCanada.js
+++ public/logic/gsapCanada.js
@@ -3,4 +3,6 @@ var rule = CSSRulePlugin.getRule("span:after");
 var tl = gsap.timeline({default: {duration: 1}})
 
 tl.from(".subtitleText", {y: -50, stagger: .6, opacity: 0})
-tl.from(".titleText", {y:-50, stagger: .6, opacity:0}, "-=0.5")
+tl.from(".titleText", {y:-150, stagger: .6, opacity:0}, "-=0.5")
+tl.from("#left-text", {x:-250, stagger: .6, opacity:0}, "-=0.5")
+tl.from("#right-text", {x:250, stagger: .6, opacity:0}, "-=0.5")
