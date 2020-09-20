fedac6e4ecb71662c7ececb66435cfb163078577
MikeTheFyke <mikefyke@hotmail.com>
2020-09-20 19:01:19 -0400

    Added right side profile pictures

--- public/logic/gsapCanada.js
+++ public/logic/gsapCanada.js
@@ -6,8 +6,8 @@ tl.from("#main-content", {y: -50, stagger: .6, opacity: 0})
 tl.from(".subtitleText", {y: -50, stagger: .6, opacity: 0})
 tl.from(".titleText", {y:-150, stagger: .6, opacity:0}, "-=0.5")
 tl.from("#left-text", {x:-250, stagger: .6, opacity:0}, "-=0.5")
-tl.from("#NTBTS-Btn", {x:-250, stagger: .6, opacity:0}, "-=0.5")
 tl.from("#right-text", {x:250, stagger: .6, opacity:0}, "-=0.5")
+tl.from("#NTBTS-Btn", {x:-250, stagger: .6, opacity:0}, "-=0.5")
 
 
 // Nirvanna The Band The Show Modal
